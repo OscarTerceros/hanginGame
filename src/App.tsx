fa4b1d4917e5fb1678b040043b4d41789062d533
Oscar Terceros <osterce@gmail.com>
2023-04-12 10:04:56 -0400

    Button cration

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-
+import { letters } from './helpers/letters';
 import './App.css'
 
 function App() {
   
   return (
     <>
-      <h1>Hola</h1>
+      {/* Imágenes */}
+      <h3>Imagen del juego</h3>
+
+      {/* Palabra oculta */}
+      <h3>_ _ _ _ _ _ _ _ _ _</h3>
+      
+      {/* Contador dew intentos */}
+      <h3>Intentos: 0</h3>
+      
+      {/* Botones de letras */}
+      { letters.map( ( letter: string, i: number ) => <button key={ i }> {letter} </button> )}
     </>
   )
 }
