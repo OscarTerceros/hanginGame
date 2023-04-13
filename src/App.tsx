c7698e95b401e06fed3d4f3019b840ec5475d3bd
Oscar Terceros <osterce@gmail.com>
2023-04-13 10:16:14 -0400

    Mensaje de ganar o perder

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { letters } from './helpers/letters';
 import { HangImage } from './components/HangImage';
 
@@ -9,9 +9,29 @@ function App() {
   const [ word, setWord ] = useState( 'COMPUTADORA' );
   const [hiddenWord, setHiddenWord] = useState( '_ '.repeat( word.length ) );
   const [ attempts, setAttempts ] = useState( 0 );
+  const [ lose, SetLose ] = useState( false );
+  const [ won, SetWon ] = useState( false );
+
+  // Determinar si la persona perdió
+  useEffect( () => {
+    if( attempts >= 9 ){
+      SetLose( true );
+    }
+  }, [attempts]);
+
+  // Determinar que la persona ganó
+  useEffect( () => {
+    const currentHiddenWord = hiddenWord.split(' ').join('');
+    if( currentHiddenWord === word ){
+      SetWon( true );
+    }
+  }, [hiddenWord]);
 
   const checkLetter = ( letter: string ) => {
     
+    if( lose ) return;
+    if( won ) return;
+
     if ( !word.includes( letter ) ){
       setAttempts( Math.min( attempts + 1, 9 ));
       return;
@@ -37,6 +57,19 @@ function App() {
       
       {/* Contador dew intentos */}
       <h3>Intentos: { attempts }</h3>
+
+      {/* Mensaje si perdió */}
+      { 
+        ( lose )
+          ? <h2>Perdio!!, la palabra es: { word }</h2>
+          :''
+      }
+      {/* Mensaje si perdió */}
+      { 
+        ( won )
+          ? <h2>Felicidades, usted ganó</h2>
+          :''
+      }
       
       {/* Botones de letras */}
       { letters.map( ( letter: string, i: number ) => <button onClick={ () => checkLetter( letter ) } key={ i }> {letter} </button> )}
